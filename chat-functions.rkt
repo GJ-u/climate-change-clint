f88ccf2f90395f951c359c6f054773e16041d4ac
GJ-u <vickarol0@gmail.com>
2021-08-31 20:41:25 +1000

    rename and split files

---
+++ chat-functions.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require "clint.rkt")
+(require "functions.rkt")
+(require "chat-variables.rkt")
+
+#| CHAT FUNCTION DEFINITIONS |#
+
+;;; utility
+
+(define (p str)
+  (printf "Clint: ~a" str))
+
+;; prompt: nil -> nil
+(define (prompt)
+  (display "\n>> ")
+  (read-line))
+
+;;; responses
+
+(define (greeting-response)
+  (printf "Clint: ~a, ~a" (string-titlecase (choose greeting))
+          (string-downcase (choose tips))))
+
+(define (greeting-and-question)
+  (printf "Clint: ~a, ~a" (string-titlecase (choose greeting))
+          (string-downcase questions)))
+
+(define (modal-affirmative [mode (choose modal-verbs)])
+  (printf "Clint: ~a ~a!" (choose affirmative-after-modal) mode))
+
+(define (modal-i [mode (choose modal-verbs-i)])
+  (printf "Clint: ~a ~a that?" (choose modal-i-response) mode))
+
+(define (ask-question)
+  (greeting-and-question)
+  (let ((answer (prompt)))
+    (begin (with-output-to-file "user.txt" #:exists 'replace
+  (lambda () (printf "~a" answer)))
+           (printf "Clint: I see. ~a" (string-downcase (choose tips))))))
+
