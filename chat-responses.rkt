2459880b73e10826e4d9032773b85c4640852d19
GJ-u <vickarol0@gmail.com>
2021-09-02 16:01:37 +1000

    update chat responses

--- chat-responses.rkt
+++ chat-responses.rkt
@@ -2,22 +2,16 @@
 
 (provide (all-defined-out))
 
-#| CHAT VARIABLE DEFINITIONS |#
+#| MISC |#
 
 (define nil '())
 
-;; used for both clint and user
-(define greeting
-  '("hello" "hi " " hi " "howdy" "hewwo there" "greetings traveller" "greetings" "what's up dog?"))
 
-(define farewell
-  '("farewell" "bye" "goodbye" "see you" "see you later" "ciao"))
+#| CLINT SPECIFIC |#
 
-;; clint specific 
 (define questions
   "how is the weather today?")
 
-;; clint specific
 (define tips
   '("remember to recycle!"
    "when shopping, make sure that you bring your own bag."
@@ -27,11 +21,6 @@
    "do you use a compost bin? Remember to compost."
    "drive efficiently today: fast acceleration, high speeds and abrupt stops waste energy."
    "remember to always opt for LED light bulbs."))
-
-
-;; Questions and responses
-(define question
-  '("who" "what" "when" "where" "how" "why"))
 
 (define who-answers
   '("??" "I don't know who."))
@@ -51,33 +40,17 @@
 (define why-answers
   '("Why do you ask?" "Why do you need to know?" "The answer may suprise you."))
 
-
-
-;; used for both clint and user
-(define modal-verbs
-  '("can" "could" "must" "should" "will" "would" "may" "ought"))
-
-(define modal-verbs-i
-  '("need" "think" "want" "have"))
-
-;; clint specific
+;; used when input contains modal verb
+;; input: you SHOULD do this -> Clint: Indeed I SHOULD 
 (define affirmative-after-modal
   '("Yes I" "Indeed I" "I sure" "I"))
 
+;; not yet used
 (define negative-after-modal
   '("No I" "Sadly I" "Sorry I"))
 
-(define modal-i-response
-  '("Since when did you" "Why do you"))
-
-(define because-words
-  '("so " "so that" "because" " so " "since"))
-
 (define because-responses
   '("Is that really so?" "That is so cool!" "Huh, interesting." "Woah, cool." "Oh really?" "That's strange."))
-
-(define user-questions
-  '("what is your name" "what do you do" "what is your purpose" "who are you" "what can you do"))
 
 (define user-question-responses
   '("I am Climate Change Clint, I provide you with tips on how to help the climate."
@@ -86,3 +59,34 @@
 
 (define generic-responses
   '("this is a generic response" "generic response!"))
+
+;; used when input contains modal i - verb
+;; input: i NEED chocolate -> Clint: Why do you NEED (that)
+;; "that" is added by the function call 
+(define modal-i-response
+  '("Since when did you" "Why do you"))
+
+
+
+#| CLINT AND USER |#
+(define modal-verbs
+  '("can" "could" "must" "should" "will" "would" "may" "ought"))
+
+(define modal-verbs-i
+  '("need" "think" "want" "have"))
+
+(define greeting
+  '("hello" "hi " " hi " "howdy" "hewwo there" "greetings traveller" "greetings" "what's up dog?"))
+
+(define farewell
+  '("farewell" "bye" "goodbye" "see you" "see you later" "ciao"))
+
+
+#| USER SPECIFIC |#
+
+(define user-questions
+  '("what is your name" "what do you do" "what is your purpose" "who are you" "what can you do"))
+
+(define because-words
+  '("so " "so that" "because" " so " "since"))
+
