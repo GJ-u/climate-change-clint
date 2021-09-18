0be73ee11343132675a3a71156a6a7e0b7de88cb
GJ-u <vickarol0@gmail.com>
2021-09-18 22:59:23 +1000

    Add startup screen

--- gui.rkt
+++ gui.rkt
@@ -38,10 +38,6 @@
 
 
 #| GUI DEFINITIONS START HERE |#
-
-(define style (make-object style-delta% 
-                'change-normal-color))
-
 ;; main window
 (define w (new frame%
                [label "Climate Change Clint"]
@@ -50,84 +46,121 @@
                [style '(fullscreen-button)]
                [alignment '(left top)]))
 
-(define const-text (new message%
-                        [label "  You are now speaking with Climate Change Clint. To set your name, type 'name'."]
-                        [parent w]
-                        [vert-margin 10]))
+(define (make-clint)
+  
+  (define style (make-object style-delta% 
+                  'change-normal-color))
 
-;; horizontal panel for alignment
-(define top-panel (new horizontal-pane%
-                       [parent w]
-                       [vert-margin 10]
-                       [horiz-margin 10]
-                       [alignment '(left bottom)]))
+  (define const-text (new message%
+                          [label "  You are now speaking with Climate Change Clint. To set your name, type 'name'."]
+                          [parent w]
+                          [vert-margin 10]))
 
-(define clint-portrait (new button%
-                            [parent top-panel]
-                            [label clint-neutral]
-                            [min-width 250]
-                            [min-height 250]))
+  ;; horizontal panel for alignment
+  (define top-panel (new horizontal-pane%
+                         [parent w]
+                         [vert-margin 10]
+                         [horiz-margin 10]
+                         [alignment '(left bottom)]))
 
-;; canvas to write text to
-(define canvas (new editor-canvas%
-                    [parent top-panel]
-                    [style '(no-focus hide-hscroll)]
-                    [min-height 250]))
+  (define clint-portrait (new button%
+                              [parent top-panel]
+                              [label clint-neutral]
+                              [min-width 250]
+                              [min-height 250]))
 
-;; define the text that will be written to the canvas
-;; used for both input and output
-(define editor (new text%))
-(send editor lock #t) ; disable typing in the editor
-(send editor auto-wrap #t) ; wrap text in the editor
+  ;; canvas to write text to
+  (define canvas (new editor-canvas%
+                      [parent top-panel]
+                      [style '(no-focus hide-hscroll)]
+                      [min-height 250]))
 
-(send canvas set-editor editor) ; set the canvas to recieve this text
+  ;; define the text that will be written to the canvas
+  ;; used for both input and output
+  (define editor (new text%))
+  (send editor lock #t) ; disable typing in the editor
+  (send editor auto-wrap #t) ; wrap text in the editor
 
-;; text-field callback
-(define (f t e)
-  (if (equal? 'text-field-enter (send e get-event-type) )
-      (begin
-        (send editor lock #f)
-        (send editor insert (string-append "\n" (send t get-value) "\n"))
-        (let ((input (string-downcase (send t get-value))))
-          ; check whether writing to file is needed
-          (cond
-            [await-weather
-             (begin0 (with-output-to-file "weather.txt" #:exists 'replace
-                       (lambda () (printf "~a" input)))
-                     (set! await-weather #f))]
-            [await-name
-             (begin0 (with-output-to-file "name.txt" #:exists 'replace
-                       (lambda () (printf "~a" input)))
-                     (set! await-name #f))]
-            [else nil])
-          ; change clint portraits at random
-          (case (random 5)
-            [(0) (send clint-portrait set-label clint-happy)]
-            [(1) (send clint-portrait set-label clint-neutral)]
-            [(2) (send clint-portrait set-label clint-goofy)]
-            [(3) (send clint-portrait set-label clint-sad)]
-            [(4) (send clint-portrait set-label clint-sob)])
-          (send txt set-value "") ; clear text field
-          ; clint's response
-          (send style set-delta-foreground "Olive")
-          (send editor change-style style)
-          (send editor insert (string-append "Clint: " (respond input)))
-          ; reset colours and lock editor
-          (send style set-delta-foreground "black")
-          (send editor change-style style)
-          (send editor lock #t)))
-      nil))
+  (send canvas set-editor editor) ; set the canvas to recieve this text
 
-; text input field
-(define txt (new text-field%
-                 [parent w]
-                 [label ">>"]
-                 [min-width 150]
-                 [min-height 30]
-                 [vert-margin 10]
-                 [horiz-margin 10]
-                 [stretchable-width #t]
-                 [stretchable-height #f]
-                 [callback f]))
+  ;; text-field callback
+  (define (f t e)
+    (if (equal? 'text-field-enter (send e get-event-type) )
+        (begin
+          (send editor lock #f)
+          (send editor insert (string-append "\n" (send t get-value) "\n"))
+          (let ((input (string-downcase (send t get-value))))
+            ; check whether writing to file is needed
+            (cond
+              [await-weather
+               (begin0 (with-output-to-file "weather.txt" #:exists 'replace
+                         (lambda () (printf "~a" input)))
+                       (set! await-weather #f))]
+              [await-name
+               (begin0 (with-output-to-file "name.txt" #:exists 'replace
+                         (lambda () (printf "~a" input)))
+                       (set! await-name #f))]
+              [else nil])
+            ; change clint portraits at random
+            (case (random 5)
+              [(0) (send clint-portrait set-label clint-happy)]
+              [(1) (send clint-portrait set-label clint-neutral)]
+              [(2) (send clint-portrait set-label clint-goofy)]
+              [(3) (send clint-portrait set-label clint-sad)]
+              [(4) (send clint-portrait set-label clint-sob)])
+            (send txt set-value "") ; clear text field
+            ; clint's response
+            (send style set-delta-foreground "Olive")
+            (send editor change-style style)
+            (send editor insert (string-append "Clint: " (respond input)))
+            ; reset colours and lock editor
+            (send style set-delta-foreground "black")
+            (send editor change-style style)
+            (send editor lock #t)))
+        nil))
+
+  ; text input field
+  (define txt (new text-field%
+                   [parent w]
+                   [label ">>"]
+                   [min-width 150]
+                   [min-height 30]
+                   [vert-margin 10]
+                   [horiz-margin 10]
+                   [stretchable-width #t]
+                   [stretchable-height #f]
+                   [callback f]))
+  nil
+  )
+
+(define alignment-a
+  (new horizontal-pane%
+       [parent w]
+       [alignment '(center bottom)]))
+
+(define alignment-b
+  (new horizontal-pane%
+       [parent w]
+       [alignment '(center top)]))
+
+(define open-text
+  (new message%
+       [parent alignment-a]
+       [label "      This is Climate Change Clint v1.0
+Climate Change Clint is licensed under GPLv3 
+    and comes with ABSOLUTELY NO WARRANTY.
+"]))
+
+(define launch-button
+  (new button%
+       [parent alignment-b]
+       [label "Launch Clint"]
+       [min-width 250]
+       [min-height 70]
+       [callback (lambda (t e)
+                   (begin
+                     (send alignment-b delete-child launch-button)
+                     (send alignment-a delete-child open-text)
+                     (make-clint)))]))
 
 (send w show #t)
