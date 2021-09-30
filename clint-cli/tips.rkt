449f6117164ec2036cc88e76b9fe946cd2bc7d74
GJ-u <vickarol0@gmail.com>
2021-09-30 22:09:27 +1000

    Add tips for next stage.

---
+++ clint-cli/tips.rkt
@@ -0,0 +1,85 @@
+#lang racket
+(define travel
+  [vector
+ "Use public transport to get to work and leisure activities. "
+ "Carpool to get to work, or to get to events that friends are also attending." 
+ "Share a car with your neighbours or friends."
+ "Reduce your car and plane travel for work – use teleconferencing."
+ "Reduce your vacation travel – take local holidays. "
+ "Park your car at a friend’s house so it’s easy not to use it every day." 
+ "Buy carbon offsets when you have to fly. "
+ "If you really need a car, buy a smaller and more efficient one, or better still, an electric or hybrid vehicle. "
+ "Put climate change stickers on your car or bike. "
+ "Maintain your car with regular tune-ups and correct air pressure. "
+ "Drive smoothly, avoid sudden acceleration."])
+
+(define home
+  [vector
+   "Use less electricity and gas – buy energy-efficient light bulbs and                         
+      appliances, turn off lights." 
+ "Insulate your house, starting with the roof space." 
+ "Wear warm jumpers and thick socks in winter and turn down your heater             
+       thermostat in winter to 18 degrees or cooler." 
+ "Cool yourself before you cool your home in summer, and turn up your air-             
+    conditioner thermostat to 24 degrees or warmer, but try to just use a fan." 
+ "Recycle everything you can."  
+ "Follow a ‘climatarian’ diet - eat less lamb and beef (they produce most               
+      methane). Compost your food waste." 
+ "Install solar power panels and solar hot water." 
+ "Buy renewable energy." 
+ "Plant trees in your garden and wherever you can. Grow your own fruit and vegetables."  
+ "Use less water – time your showers, install rainwater tanks."  
+ "Put up signs on your garden fence or letterbox about climate change." 
+ "If you're building, use an eco-friendly design and builder." 
+ "Replace heating and cooling units with more efficient models." 
+"When you move house, go small - prioritise energy efficiency and public               
+      transport." ])
+
+(define work
+  [vector
+   "Join or form a green team at work."
+   "Ask your workplace to divest from companies that invest in fossil fuels."
+   "Develop a sustainability policy with guidelines for energy efficiency, paper use, procurement policies, catering, recycling, energy providers etc."
+   "Make your environmental values clear to your colleagues."
+   "Ride a bike or walk to work."])
+
+(define nature
+  [vector
+   "Spend time in the natural places that you love."
+   "Notice the wild, natural world around you, wherever you are."
+   "Spend time with others in nature, or join a social group with shared interests like tree planting, weed removal, or bird watching."
+   "Donate your money or time to nature conservation activities."])
+
+(define shopping
+  [vector
+   "If you can, buy fewer things."
+   "Walk or ride to the shops."
+   "Refuse plastic bags when you shop, and tell them why. Take your own bags instead!"
+   "Buy local, organic, seasonal food."
+   "Buy recycled goods."
+   "Get hold of a sustainable supermarket guide and be a wise consumer."
+   "Avoid buying products in non-recyclable containers."
+   "Take your money out of banks and other financial institutions that support the fossil fuel indrustry, and tell them why you are doing so."
+   "Refuse to buy bottled water. Take your own water bottle with you, and a travel cup for take-away coffee."
+   "When you can, choose to eat in the restaurant rather than getting takeaway."
+   "When 'treating yourself', consider choosing services rather than buying more 'stuff'."])
+
+(define communication
+  [vector
+   "Remind your friends that there is still time to take action on climate change and prevent the worst harms."
+   "Show other people that it is normal to care about the environment."
+   "Tell stories of people doing positive things for the environment."
+   "Be obvious about your environmental values."
+   "Break the silence on climate change whenever possible."
+   "Encourage other people to express their concerns about climate change, and make it 'normal' to do so."
+   "Get your school or college to start projects on climate change."
+   "If you use social media, be vocal about your climate concerns. If you do not, be vocal about your concerns when speaking to others."
+   "Put up posters on climate change at your school or workplace."
+   ])
+
+(define education
+  [vector
+   "Plant a tree with young children to teach them about the climate."
+   "Let kids walk or ride to school if it is possible."
+   "Show children how you are working to protect the climate so that they have a better world to grow up in."
+   "Encourage your school to engage students in climate change projects such as tree planting, letters to politicians about their hopes and fears for the future, putting on a play or concert about climate change, or studying the effects of global heating."])
